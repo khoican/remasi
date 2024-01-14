f746eaa1e952d5cef6ae724505aeb4487c269cf0
khoican <ibnukhoirulprasetyo@gmail.com>
2024-01-14 14:01:59 +0700

    perf: create feature access account for admin

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,16 @@
-<header class="d-flex justify-content-between align-items-center px-5 py-3">
+<header class="d-flex justify-content-between align-items-center px-5 py-4">
     <h1 class="fs-3 fw-semibold">
         @yield('header')
     </h1>
 
-    <a href="{{ route('logout') }}" class="d-flex flex-row gap-3 align-items-center btn btn-outline-danger">
-        <i class="bi bi-box-arrow-right"></i> Logout
-    </a>
+    <div class="dropdown">
+        <button class="btn btn-success fw-semibold dropdown-toggle d-flex align-items-center gap-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-circle fs-6"></i>
+            {{ Auth::user()->name }}
+        </button>
+        <ul class="dropdown-menu dropdown-menu-end">
+            <li><a class="dropdown-item" href="{{ route('change_password') }}">Ganti Password</a></li>
+            <a href="{{ route('logout') }}" class="dropdown-item text-danger">Logout</a>
+        </ul>
+    </div>
 </header>
